5bb09706a4f5041227a766ab1e6d0655e2bb7982
yberfox228 <domvlielupe3@gmail.com>
2018-09-10 20:19:06 +0300

    starting with css animations

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 
-
 var Clock = document.querySelector(".dial");
 var ClockWidth = Clock.clientWidth;
 var ClockHeight = Clock.clientHeight;
@@ -16,7 +15,18 @@ if (ClockWidth < ClockHeight) {
     dialRadius = ClockHeight/2
 };
 
+secondsX = (ClockWidth/2);
+secondsY = (ClockHeight/2);
 
+
+
+//--------------
+/*
+var seconds = sClock.rect((ClockWidth/2), (ClockHeight/2), (dialRadius-5), (dialRadius*0.02), 5);
+seconds.attr({
+    fill: 'red'
+})
+*/
 var dial = sClock.circle((ClockWidth/2), (ClockHeight/2), dialRadius);
 
 dial.attr({
@@ -24,31 +34,35 @@ dial.attr({
 
 });
 
-//var secondsXcoord = ((ClockHeight/2) + ((dialRadius*0.02)/2)
-//--------------
-var seconds = sClock.rect((ClockWidth/2), (ClockHeight/2), (dialRadius-5), (dialRadius*0.02), 5);
-seconds.attr({
-    fill: 'red'
+var secondsHand = sClock.line (secondsX, secondsY, (secondsX+(dialRadius*0.95)), secondsY);
+
+secondsHand.attr({
+    strokeWidth: 2,
+    stroke: "red",
+    strokeLinecap: "round",
+});
+
+secondsHand.hover(function(){
+    secondsHand.animate({transform: "r30,"+ secondsX + ","+secondsY}, 1000)
 })
 
-
+/*
 var Mat=new Snap.Matrix();
-Mat.rotate(30,(ClockWidth/2), (ClockHeight/2));
-secondsX = (ClockWidth/2);
-secondsY = (ClockHeight/2);
-
+Mat.rotate(30, secondsX, secondsY);
+*/
 /*
 seconds.hover(function(){
 
     seconds.animate({transform: "r30,"+ secondsX + ","+secondsY}, 1000);
 })
 */
+/*
 var updateTime = function(){
     seconds.animate({transform: "r30,"+ secondsX + ","+secondsY}, 500);
 
 };
 setInterval(updateTime(), 1000);
-
+*/
 
 
 
